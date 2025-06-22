96a7c7de897c478bf55cd00ae62a65d8691abba4
Akkurt Bey <doganalp.arslan@outlook.com>
2025-06-23 02:14:30 +0300

    Dialogue Rescue Trial
    Dialogue Manager update'i sonrası bozulan kısımları geri döndürdüm fakat eski dialogue dosyalarını yenilemek için şimdi bunları yolluyorum, umarım çalışır.

--- text-based-rpg-demo/scripts/game_state.gd
+++ text-based-rpg-demo/scripts/game_state.gd
@@ -29,9 +29,61 @@ func update_nance_lines(number):
 
 
 # --------------------------------------CASEY
+<<<<<<< Updated upstream
 var casey_relation: int= 0
 
 
 func casey_relationship(casey_number):
 	casey_relation += casey_number
 	print_debug("Casey relation: " + str(casey_number))
+=======
+var has_met_casey: bool = false
+
+var casey_relation: int= 0:
+	set(casey_number):
+		casey_relation = casey_number
+		print_debug("Casey_relation: " + str(casey_relation))
+
+var casey_prog: int= 0:
+	set(progress_number):
+		casey_prog = progress_number
+		print_debug("Casey_prog: " + str(casey_prog))
+
+var casey_story_told = 0:
+	set(value):
+		print_debug("casey_story_told: " + str(value))
+
+
+
+# --------------------------------------CHROME
+var has_met_chrome: bool = false
+
+
+
+
+func check_bool(variable, number) -> bool:
+	if variable == number:
+		print_debug("check_bool: true") 
+		return true
+	else:
+		print_debug("check_bool: false")
+		return false
+
+
+#func casey_relationship(casey_number):
+	#casey_relation += casey_number
+	#print_debug("Casey relation: " + str(casey_number))
+
+#func casey_progress(progress_number):
+	#casey_prog += progress_number
+	#print_debug("Casey relation: " + str(progress_number))
+
+#func romanced_nance(amount):
+	#nance_flirted += amount
+	#print_debug("flirted nance: " + str(amount))
+
+#func update_nance_lines(number):
+	#nance_lines_created = number
+	#print_debug("nance_lines_created= " + str(nance_lines_created))
+	#print_debug("lines_created= " + str(lines_created))
+>>>>>>> Stashed changes
