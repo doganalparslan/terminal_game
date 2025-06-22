ad2d870dce2e4d66be0bee8ba47dff7e094d8c3b
Akkurt Bey <doganalp.arslan@outlook.com>
2025-06-23 02:09:44 +0300

    Revert "21-06-25"
    This reverts commit 383988c099436360c4b770f3143b19c5e1a59a8f.

--- text-based-rpg-demo/scripts/game_state.gd
+++ text-based-rpg-demo/scripts/game_state.gd
@@ -13,63 +13,25 @@ var lines_created: int = 0: # TO KEEP TRACK OF PLAYER SPENT TIME // USE EXAGGARA
 
 var never_see_line = false
 
-
-
 # --------------------------------------NANCE
 var has_met_nance: bool = false
+var nance_flirted: int = 0
+var nance_lines_created: int = 0
 
-var nance_flirted: int = 0:
-	set(amount):
-		print_debug("flirted nance: " + str(amount))
+func romanced_nance(amount):
+	nance_flirted += amount
+	print_debug("flirted nance: " + str(amount))
 
-var nance_lines_created: int = 0:
-	set(number):
-		nance_lines_created = number
-		print_debug("nance_lines_created= " + str(nance_lines_created))
-		print_debug("lines_created= " + str(lines_created))
-
+func update_nance_lines(number):
+	nance_lines_created = number
+	print_debug("nance_lines_created= " + str(nance_lines_created))
+	print_debug("lines_created= " + str(lines_created))
 
 
 # --------------------------------------CASEY
-var has_met_casey: bool = false
-
-var casey_relation: int= 0:
-	set(casey_number):
-		casey_relation += casey_number
-		print_debug("Casey relation: " + str(casey_number))
-
-var casey_prog: int= 0:
-	set(progress_number):
-		casey_prog += progress_number
-		print_debug("Casey relation: " + str(progress_number))
-
-var casey_story_told = 0:
-	set(value):
-		print_debug("casey_story_told: " + str(value))
+var casey_relation: int= 0
 
 
-
-
-
-
-
-
-
-
-
-#func casey_relationship(casey_number):
-	#casey_relation += casey_number
-	#print_debug("Casey relation: " + str(casey_number))
-
-#func casey_progress(progress_number):
-	#casey_prog += progress_number
-	#print_debug("Casey relation: " + str(progress_number))
-
-#func romanced_nance(amount):
-	#nance_flirted += amount
-	#print_debug("flirted nance: " + str(amount))
-
-#func update_nance_lines(number):
-	#nance_lines_created = number
-	#print_debug("nance_lines_created= " + str(nance_lines_created))
-	#print_debug("lines_created= " + str(lines_created))
+func casey_relationship(casey_number):
+	casey_relation += casey_number
+	print_debug("Casey relation: " + str(casey_number))
