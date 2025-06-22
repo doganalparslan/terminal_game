ff31e3785a14a0b65b683c666bebc27355266c01
Akkurt Bey <doganalp.arslan@outlook.com>
2025-06-23 02:30:53 +0300

    Manual Push of 21-06-25 again
    Files conflicted and push was unavailable so I am manually re-uploading it after downloading from github website

--- text-based-rpg-demo/scripts/game_state.gd
+++ text-based-rpg-demo/scripts/game_state.gd
@@ -13,41 +13,35 @@ var lines_created: int = 0: # TO KEEP TRACK OF PLAYER SPENT TIME // USE EXAGGARA
 
 var never_see_line = false
 
+
+
 # --------------------------------------NANCE
 var has_met_nance: bool = false
-var nance_flirted: int = 0
-var nance_lines_created: int = 0
 
-func romanced_nance(amount):
-	nance_flirted += amount
-	print_debug("flirted nance: " + str(amount))
+var nance_flirted: int = 0:
+	set(amount):
+		print_debug("flirted nance: " + str(amount))
 
-func update_nance_lines(number):
-	nance_lines_created = number
-	print_debug("nance_lines_created= " + str(nance_lines_created))
-	print_debug("lines_created= " + str(lines_created))
+var nance_lines_created: int = 0:
+	set(number):
+		nance_lines_created = number
+		print_debug("nance_lines_created= " + str(nance_lines_created))
+		print_debug("lines_created= " + str(lines_created))
+
 
 
 # --------------------------------------CASEY
-<<<<<<< Updated upstream
-var casey_relation: int= 0
-
-
-func casey_relationship(casey_number):
-	casey_relation += casey_number
-	print_debug("Casey relation: " + str(casey_number))
-=======
 var has_met_casey: bool = false
 
 var casey_relation: int= 0:
 	set(casey_number):
-		casey_relation = casey_number
-		print_debug("Casey_relation: " + str(casey_relation))
+		casey_relation += casey_number
+		print_debug("Casey relation: " + str(casey_number))
 
 var casey_prog: int= 0:
 	set(progress_number):
-		casey_prog = progress_number
-		print_debug("Casey_prog: " + str(casey_prog))
+		casey_prog += progress_number
+		print_debug("Casey relation: " + str(progress_number))
 
 var casey_story_told = 0:
 	set(value):
@@ -55,19 +49,12 @@ var casey_story_told = 0:
 
 
 
-# --------------------------------------CHROME
-var has_met_chrome: bool = false
 
 
 
 
-func check_bool(variable, number) -> bool:
-	if variable == number:
-		print_debug("check_bool: true") 
-		return true
-	else:
-		print_debug("check_bool: false")
-		return false
+
+
 
 
 #func casey_relationship(casey_number):
@@ -86,4 +73,3 @@ func check_bool(variable, number) -> bool:
 	#nance_lines_created = number
 	#print_debug("nance_lines_created= " + str(nance_lines_created))
 	#print_debug("lines_created= " + str(lines_created))
->>>>>>> Stashed changes
